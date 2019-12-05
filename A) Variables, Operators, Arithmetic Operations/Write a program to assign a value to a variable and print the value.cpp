299f1aecfd3d9a7667153a35caba3f83050ae52d
Smit <heysmitbarmase@gmail.com>
2019-12-05 21:29:55 +0530

    First Commit

---
+++ A) Variables, Operators, Arithmetic Operations/Write a program to assign a value to a variable and print the value.cpp
@@ -0,0 +1,12 @@
+//Write a program to assign a value to a variable and print the value.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x;
+  cout<<"Enter a value : ";
+  cin>>x;
+  cout<<"The value you entered is : "<<x<<endl;
+  return 0;
+}
