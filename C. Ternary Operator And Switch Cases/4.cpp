21f6b03b68a3934e4d7bd274c34735ff157f3b03
Smit <heysmitbarmase@gmail.com>
2019-12-24 15:48:22 +0530

    Leap year program redefined.

--- C. Ternary Operator And Switch Cases/4.cpp
+++ C. Ternary Operator And Switch Cases/4.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main()
 {
-  int x;
+  int x, leap=0;
   cout<<"Enter a year : ";
   cin>>x;
   cout<<x<<" is ";
-  x%4==0 ? cout<<"a leap year." : cout<<"not a leap year.";
+  x%4==0 && x%100!=0 ? leap=1 : (x%400==0 ? leap=1 : leap=0));
+  leap==1 ? cout<<"This is leap year. " : cout<<"This is not a leap year.";
   return 0;
 }
